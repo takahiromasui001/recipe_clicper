9878fd4431bed5226ed58a22581eda8cacb05d7c
Takahiro Masui <takahiro.masui@luxiar.com>
2023-05-01 12:10:43 +0900

    一覧画面レイアウト

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Recipe.create(
+  [
+    { name: '豚とナスの甘辛うまみ炒め', url: 'https://tuberecipe.com/recipe/4041/detail' },
+    { name: 'サムギョプサル', url: 'https://tuberecipe.com/recipe/5185/detail' },
+    { name: '虚無トマトパスタ', url: 'https://pieceke.com/cooking/kyomu-tomato-pasta/' },
+    { name: '至高のポテトグラタン', url: 'https://tuberecipe.com/recipe/10990/detail' },
+  ],
+)
