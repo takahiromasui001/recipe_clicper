370f3df0cdd3b4d02ae05311f100243c0c92ef87
Takahiro Masui <takahiro.masui@luxiar.com>
2023-05-02 16:25:44 +0900

    new処理

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,22 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    raise unless @recipe.save!
+
+    redirect_to recipes_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :url)
+  end
 end
