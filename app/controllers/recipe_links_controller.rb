11c36020c202a8d5b6315760b41c7926a80ded1a
Takahiro Masui <takahiro.masui@luxiar.com>
2023-05-19 20:59:51 +0900

    削除処理の追加

--- app/controllers/recipe_links_controller.rb
+++ app/controllers/recipe_links_controller.rb
@@ -1,6 +1,9 @@
 class RecipeLinksController < ApplicationController
   def create
     @recipe = Recipe.find(params[:recipe_id])
-    render turbo_stream: turbo_stream.append(:recipe_links, partial: 'recipe_links/recipe_link', locals: { recipe: @recipe })
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
   end
 end
